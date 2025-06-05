1de3c5d6016b7cb665ef9c81f79df86ac029f6e3
Dmytro Dzhemchuk <dzhemchuk@gmail.com>
2025-06-05 19:17:00 -0400

    Complete Exercise 6

--- part6-exercise2/Views/MoviesListPage.xaml.cs
+++ part6-exercise2/Views/MoviesListPage.xaml.cs
@@ -11,11 +11,4 @@ public partial class MoviesListPage : ContentPage
     {
         await Navigation.PushAsync(new Views.MovieDetailPage());
     }
-
-    private void MenuItem_Clicked(object sender, EventArgs e)
-    {
-        MenuItem menuItem = (MenuItem)sender;
-        ViewModels.MovieViewModel movie = (ViewModels.MovieViewModel)menuItem.BindingContext;
-        App.MainViewModel?.DeleteMovie(movie);
-    }
 }
