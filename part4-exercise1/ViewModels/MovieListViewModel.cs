9c16474fb2b7fbfc95ea9bc0a972228abe5832c3
bens0101 <156142599+bens0101@users.noreply.github.com>
2025-06-05 23:50:11 -0400

    finished the assignment

--- part4-exercise1/ViewModels/MovieListViewModel.cs
+++ part4-exercise1/ViewModels/MovieListViewModel.cs
@@ -1,23 +1,39 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MovieCatalog.ViewModels;
 
-public class MovieListViewModel: ObservableObject
+public partial class MovieListViewModel : ObservableObject
 {
     public ObservableCollection<MovieViewModel> Movies { get; set; }
 
-    public MovieListViewModel() =>
+    private MovieViewModel _selectedMovie;
+    public MovieViewModel SelectedMovie
+    {
+        get => _selectedMovie;
+        set => SetProperty(ref _selectedMovie, value);
+    }
+
+    public ICommand DeleteMovieCommand { get; }
+
+    public MovieListViewModel()
+    {
         Movies = [];
+        DeleteMovieCommand = new RelayCommand<MovieViewModel>(DeleteMovie);
+    }
 
     public async Task RefreshMovies()
     {
         IEnumerable<Models.Movie> moviesData = await Models.MoviesDatabase.GetMovies();
-
         foreach (Models.Movie movie in moviesData)
             Movies.Add(new MovieViewModel(movie));
     }
 
-    public void DeleteMovie(MovieViewModel movie) =>
-        Movies.Remove(movie);
+    public void DeleteMovie(MovieViewModel movie)
+    {
+        if (Movies.Contains(movie))
+            Movies.Remove(movie);
+    }
 }
--- part4-exercise1/Views/MovieDetailPage.xaml.cs
+++ part4-exercise1/Views/MovieDetailPage.xaml.cs
@@ -2,9 +2,9 @@ namespace MovieCatalog.Views;
 
 public partial class MovieDetailPage : ContentPage
 {
-	public MovieDetailPage(ViewModels.MovieViewModel movie)
+	public MovieDetailPage()
 	{
-		BindingContext = movie;
 		InitializeComponent();
+		BindingContext = App.MainViewModel.SelectedMovie;
 	}
 }
