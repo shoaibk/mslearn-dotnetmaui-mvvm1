dcba1b05a6d0670e19a8004defbd30bd4a5810ac
Asif Ashadullah <156141923+asha0012-asif@users.noreply.github.com>
2025-06-08 15:06:55 -0400

    complete exercise part 1 - manage selection by using a viewmodel

--- part4-exercise1/Views/MovieDetailPage.xaml.cs
+++ part4-exercise1/Views/MovieDetailPage.xaml.cs
@@ -2,9 +2,9 @@ namespace MovieCatalog.Views;
 
 public partial class MovieDetailPage : ContentPage
 {
-	public MovieDetailPage(ViewModels.MovieViewModel movie)
-	{
-		BindingContext = movie;
-		InitializeComponent();
-	}
+    public MovieDetailPage()
+    {
+        BindingContext = App.MainViewModel.SelectedMovie;
+        InitializeComponent();
+    }
 }
